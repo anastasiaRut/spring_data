1ed2c6d21f0fec6a42ae7fdc6d6deb6539f5b1b7
Anastasia <asyaruta@gmail.com>
2019-04-08 14:22:57 +0300

    Configure Spring MVC

---
+++ school/src/main/java/com/it/config/DispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package com.it.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
